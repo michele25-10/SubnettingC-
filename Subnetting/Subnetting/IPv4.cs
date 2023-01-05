ee37906cf282f66b74afa8eaedcc92724ee5eaad
michele25-10 <michele.gabri01@gmail.com>
2023-01-05 19:25:48 +0100

    ProgettoVacanzeNatalizie

---
+++ Subnetting/Subnetting/IPv4.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Subnetting
+{
+    class IPv4
+    {
+        protected byte [] ipAddress;
+        protected byte [] subnetMask;
+
+        public IPv4()
+        {
+        }
+
+        public byte[] GetIP_addr()
+        {
+            return this.ipAddress;
+        }
+
+        public string GetIP_addbool()      // ritorna l'ip in formato binario
+        {
+            string IPbinario = "";
+            foreach(byte oct in this.ipAddress)
+            {
+                IPbinario += Convert.ToString(oct, 2).PadLeft(8, '0') + " ";
+                //ToString(oct, 2) --> converte in binario
+                //PadLeft(8,'0')--> Riempie l'ottetto nel caso in cui sia inferiore ad 8 del carattere '0' 
+            }
+
+            return IPbinario;
+        }
+
+        public byte[] GetSubnetMask()
+        {
+            return this.subnetMask;     //Torno la subnet
+        }
+
+        public bool CheckSubOct(byte octect)
+        {
+            string binarioSub = Convert.ToString(octect, 2);    //Converto in binario la stringa digitata
+            if (binarioSub.Length > 1)                          //Se la lunghezza della stringa è maggiore di uno
+            {
+                for (int i = 0; i < 7; i++)
+                {
+                    if (binarioSub[i] == '0' && binarioSub[i + 1] == '1')       //se trovo uno 01111111 --> allora errore non può essere subnet mask
+                    {
+                        return false;           
+                    }
+                }
+            }
+            return true;
+        }
+
+        public bool checkFullSub()          
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                if (this.subnetMask[i] < this.subnetMask[i+1])
+                {
+                    //Se l'ottetto successivo è maggiore del precedente torna un errore
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public void SetIp_address(byte[] Indirizzoip)        //Setto variabili della classe ipv4
+        {
+            this.ipAddress = Indirizzoip;
+        }
+
+        public void setSubnetMask(byte[] SubnetMask)        //Setto variabili della classe ipv4
+        {
+            this.subnetMask = SubnetMask;
+        }
+
+        public byte[] GetNetworkAddress()
+        {
+            byte[] networkAddress = new byte[4];
+
+            for (int i = 0; i < this.ipAddress.Length; i++)
+            {
+                networkAddress[i] = (byte)(this.ipAddress[i] & this.subnetMask[i]);         //tipo di dato in uscita è un byte,Operatore logico and tra subnet mask e inidirizzi nei vari otteti
+            }
+            return networkAddress;
+        }
+
+        public byte[] GetBroadcast()
+        {
+            byte[] broadcastAddress = new byte[4];
+
+            for (int i = 0; i < this.ipAddress.Length; i++)
+            {
+                broadcastAddress[i] = (byte)(this.GetNetworkAddress()[i] | this.GetWildcard()[i]);      //operazione di or tra inidirizzo Ip e wild mask
+            }
+            return broadcastAddress;
+        }
+
+        public double GetTotalNumberHost()
+        {
+            return Math.Pow(2, Convert.ToDouble(this.GetCIDR().Count(bitHost => bitHost == '0')));
+
+            //Elevo al quadrato
+            //il getCidr e conto dove bitHost == 0 e sarà il numero che verrà elevato al quadrato
+        }
+
+        public double GetNumberUsableHost()
+        {
+            return this.GetTotalNumberHost() - 2;       
+            
+            //Tolgo 2 al numero totale di host dal momento che quest'ultimi sono broadcast ed indirizzo di rete
+        }
+
+        public byte[] GetWildcard() 
+        {
+            string tmp = "";
+            byte[] wildcard = new byte[4];
+            int i=0;
+
+            //Definisco le variabili
+
+            foreach (byte oct in this.subnetMask)
+            {
+                tmp += Convert.ToString(oct, 2).Replace('0', '~').Replace('1', '0').Replace('~', '1').PadLeft(8,'1');           
+
+                //Converte in binario l'ottetto in esame, rimpiazzo i vari caratteri con i numeri prestabiliti.
+                //Se non arrivo a 8 caratteri aggiungo 1 fino ad arrivarci a partire da sinistra
+
+                wildcard[i] = Convert.ToByte(tmp.Substring(8 * i, 8), 2);
+
+                //converto in byte la stringa tmp
+                //Applico il metodo SubString per prelevare delle sotto stringhe composte da 8 bit, e partendo dalla posizione 8*i
+                //Ritorno il byte in binario
+
+                i++;
+
+                //Incremento la nostra variabile contatore
+            }
+            return wildcard;
+        }
+
+        public string GetCIDR() // ritorna maschera in bit
+        {
+            string SubnetBinario = "";
+
+            foreach (byte oct in this.subnetMask)
+            {
+                SubnetBinario += Convert.ToString(oct, 2).PadLeft(8, '0') + " "; 
+                //ToString(oct, 2) --> converte in binario
+                //PadLeft(8,'0')--> Riempie l'ottetto nel caso in cui sia inferiore ad 8 del carattere '0' 
+            }
+
+            return SubnetBinario;
+        }
+
+        public byte[] GetFirstHostIP() 
+        {
+            byte[] firstIpAddress = new byte[4];
+            firstIpAddress = this.GetNetworkAddress();
+            firstIpAddress[3] += 1;
+
+            //All'indirizzo di rete sommo uno nell'ultimo ottetto
+
+            return firstIpAddress;
+        }
+
+        public byte[] GetLastHostIP() 
+        {
+            byte[] lastIpAddress = new byte[4];
+            lastIpAddress = this.GetBroadcast();
+            lastIpAddress[3] -= 1;
+
+            //Tolgo uno al broadcast nell'ultimo otteto
+
+            return lastIpAddress;
+        }
+
+        public string toString()
+        {
+            return $"Ip address: {this.ipAddress[0]}.{this.ipAddress[1]}.{this.ipAddress[2]}.{this.ipAddress[3]}\nSubnet Mask: {this.subnetMask[0]}.{this.subnetMask[1]}.{this.subnetMask[2]}.{this.subnetMask[3]}";
+        }
+    }
+}
---
+++ Subnetting/Subnetting/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Subnetting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] ip = new byte[4];
+            byte[] sm = new byte[4];
+
+            bool check;
+            string ottetto;
+
+            IPv4 ipv4 = new IPv4();         //Dichiarazione classe
+
+            for (int i = 0; i < 4; i++)     //Inserisco i 4 ottetti del primo Ip
+            {
+                do
+                {
+                    Console.WriteLine($"Inserisci il {i + 1} otteto dell'ip");
+                    ottetto = Convert.ToString(Console.ReadLine());
+                } while (!Byte.TryParse(ottetto, out ip[i]));     //Controllo sull'ottetto inserito che non sia maggiore del previsto
+            }
+            ipv4.SetIp_address(ip);     //Setto le variabili della classe
+
+            Console.WriteLine("\n\n\n\n");
+
+            do
+            {
+                for (int i = 0; i < 4; i++)     //Inse risco i 4 ottetti della subnet mask
+                {
+                    do
+                    {
+                        Console.WriteLine($"Inserisci il {i + 1} otteto della subnet mask");
+                        ottetto = Convert.ToString(Console.ReadLine());
+                        check = Byte.TryParse(ottetto, out sm[i]);
+                    } while (!check || !ipv4.CheckSubOct(sm[i]));   //Ripeti finche il contollo non è true e controllo sulla subnet mask
+                }
+                ipv4.setSubnetMask(sm);         //Setto la variabile
+            } while (!ipv4.checkFullSub());     //Cotrollo sull'intera subnet mask
+
+            Console.Clear();
+
+            //Comunico in output i risultati presi dall'ipv4
+            Console.WriteLine($"\n{ipv4.toString()}\n");
+
+            Console.WriteLine("-----------------------------------------------------------\n");
+
+            Console.WriteLine($"Ip in bit: {ipv4.GetIP_addbool()}\n");
+            Console.WriteLine($"Subnet Mask in bit: {ipv4.GetCIDR()}\n\n");
+            Console.WriteLine($"Indirizzo di rete: {String.Join(".",ipv4.GetNetworkAddress())}\n");
+            Console.WriteLine($"Indirizzo di broadcast: {String.Join(".", ipv4.GetBroadcast())}\n");
+            Console.WriteLine($"Wildcard mask: {String.Join(".", ipv4.GetWildcard())}\n\n");
+            Console.WriteLine($"Numero totale degli host: {ipv4.GetTotalNumberHost()}\n");
+            Console.WriteLine($"Numero degli host utilizzabili: {ipv4.GetNumberUsableHost()}\n");
+            Console.WriteLine($"Range: {String.Join(".", ipv4.GetFirstHostIP())} - {String.Join(".", ipv4.GetLastHostIP())}\n");
+
+            Console.ReadKey();
+        }
+    }
+}
